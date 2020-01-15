1a4c8cf5e0ee369cc1545823bda95b7d1a5a9287
Tatiana Leon <tatiana.leon.mont@gmail.com>
2020-01-15 17:05:27 -0500

    Beginnig with conditional expressions

---
+++ Primeros pasos/conditionalExpressions.py
@@ -0,0 +1,21 @@
+# Example 1
+
+'''number=int(input("Insert a number: "))
+
+if number>0:
+    print("The number is positive")
+elif number == 0:
+    print("The number is zero")
+else:
+    print("The number is negative")
+print("End of the program")'''
+
+# mixed conditional
+'''
+age = int(input("Insert your age: "))
+if 0 < age < 100:
+    print("Correct age")
+    if age >= 18:
+        print("Is over age")
+else:
+    print("Incorrect age")'''
