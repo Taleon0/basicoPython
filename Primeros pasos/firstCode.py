847496239888ea59a4bcf9513f3f50be1e91f457
Tati Leon <yerly.leon@cun.edu.co>
2020-01-14 19:38:37 -0500

    Kept to 6 Video

---
+++ Primeros pasos/firstCode.py
@@ -0,0 +1,20 @@
+# Numeric or text create automatically depends of the type
+number = 10  # print(type(number)) report type int
+number = 10.56  # print(type(number)) report type float
+chain = "Hola hola !!!"  # print(type(chain)) report type string
+value = True  # print(type(value)) report type Boolean
+
+# Examples of operations
+n1 = 10
+n2 = 5.8
+nT = (n1 + n2) * 18 / 2
+# print("The answer is ", nT)
+
+# Dynamic typing : change the type of value in the same variable
+number = "now I'm a String!"
+# print(number)
+
+'''Comment
+    multi
+    linea'''
+
