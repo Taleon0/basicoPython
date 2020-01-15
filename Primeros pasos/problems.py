7d8f8492e52eb94c6469ffa1a4ac07f3fb1b443f
Tati Leon <yerly.leon@cun.edu.co>
2020-01-14 21:13:17 -0500

    Exercises based on firstCode

---
+++ Primeros pasos/problems.py
@@ -0,0 +1,16 @@
+# Exercise number 1
+
+# a = float(input("Write a value: "))
+# b = float(input("Write b value: "))
+# c = float(input("Write c value: "))
+# answer = (a**3 * (b**2 - 2*a*c))/(2*b)
+# print(f"The answer is : {answer}")
+
+
+# Exercise number 2
+
+a = float(input("Write a value: "))
+b = float(input("Write b value: "))
+answer = ((3 + 5 * 8) < 3 and ((-6 / 3 * 4)+2 < 2)) or (a > b)
+#               43 F                -8 T                   3 T
+print(f"The answer is : {answer}")
