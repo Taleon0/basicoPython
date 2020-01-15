98f8a74144967fc5d9d023f5cc61fdb271e9cbf1
Tati Leon <yerly.leon@cun.edu.co>
2020-01-14 20:34:03 -0500

    Saved until the tenth class

--- Primeros pasos/firstCode.py
+++ Primeros pasos/firstCode.py
@@ -4,12 +4,6 @@ number = 10.56  # print(type(number)) report type float
 chain = "Hola hola !!!"  # print(type(chain)) report type string
 value = True  # print(type(value)) report type Boolean
 
-# Examples of operations
-n1 = 10
-n2 = 5.8
-nT = (n1 + n2) * 18 / 2
-# print("The answer is ", nT)
-
 # Dynamic typing : change the type of value in the same variable
 number = "now I'm a String!"
 # print(number)
@@ -17,4 +11,61 @@ number = "now I'm a String!"
 '''Comment
     multi
     linea'''
+
+# Examples of operations
+n1 = 10
+n2 = 3
+sum_ = n1 + n2
+remainder_ = n1 - n2
+multiplication_ = n1 * n2
+division_ = n1 / n2
+division_to_down = n1 // n2
+module_ = n1 % n2
+potency_ = n1 ** n2
+nT = (n1 + n2) * 18 / 2
+exNumber = 3 ** 3 * (13 / 5 - (2 * 4))
+# print("The answer is ", nT)
+
+# Relational operations : return boolean answers
+# <, >, =, <=, >=, !=, ==
+result = 10 == 20
+# print(nT >= exNumber)
+# print(result)
+
+# Logical operations: and / or / not
+# evaluation order --> not -> and -> or
+a = 10
+b = 12
+c = 13
+d = 10
+# print(((a > b) or (a < c)) and ((a == c) or (a >= b)))
+#        F           T              F            F
+#               T                        F
+
+''' Priority of operations:
+    1. ()
+    2. **
+    3. *, /, mod, not
+    4. +, -, and
+    5. <, >, ==, >=, <=, !=, or
+'''
+b = 15
+c = 20
+result = not ((a < b) and (b < c))
+# print(result)
+
+# Assign operations
+a = 0
+a += 5  # sum in assignment
+a -= 2  # subtraction in assignment
+a *= 3  # multiplication in assignment
+a /= 3  # Division in assignment
+a **= 2  # Potency in assignment
+a %= 2  # module in assignment
+
+# Output by console
+by = "Tati"
+created = "January"
+print("Python course by {} created in {}".format(by, created))
+print(f"Python course by {by} created in {created}")
 
